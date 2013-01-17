dee66920492844d69d25c5373df0d0a7cd86eb99
Joe Baldwin <joe@humboldttechgrou.com>
2013-01-17 13:21:08 -0600

    OpenGL transformations

---
+++ OpenGLTree/OpenGLTree/testAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  testAppDelegate.h
+//  OpenGLTree
+//
+//  Created by Joseph on 1/17/13.
+//  Copyright (c) 2013 Joe Baldwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class testViewController;
+
+@interface testAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) testViewController *viewController;
+
+@end
---
+++ OpenGLTree/OpenGLTree/testViewController.h
@@ -0,0 +1,14 @@
+//
+//  testViewController.h
+//  OpenGLTree
+//
+//  Created by Joseph on 1/17/13.
+//  Copyright (c) 2013 Joe Baldwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface testViewController : GLKViewController
+
+@end
