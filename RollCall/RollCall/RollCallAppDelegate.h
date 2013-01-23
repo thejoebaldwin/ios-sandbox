8f33a2f506186d7bef8908f0a8351f88942e0ac7
Joe Baldwin <joe@humboldttechgrou.com>
2013-01-23 12:22:13 -0600

    Proof-of-life on roll call app

---
+++ RollCall/RollCall/RollCallAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RollCallAppDelegate.h
+//  RollCall
+//
+//  Created by Joseph on 1/23/13.
+//  Copyright (c) 2013 Joe Baldwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RollCallAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RollCall/RollCall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RollCall
+//
+//  Created by Joseph on 1/23/13.
+//  Copyright (c) 2013 Joe Baldwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RollCallAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RollCallAppDelegate class]));
+    }
+}
